f3e2894fa3a832e13f92505a5a37d1bc3ae89b2f
bym24v <manuel15986@hotmail.com>
2015-12-02 13:13:16 +0100

    server.js

---
+++ MawLab_server/server.js
@@ -0,0 +1,22 @@
+// Modulos
+//var geoIP = require("./core/geoIP.js");
+//var dnsResolver = require("./core/dnsResolver.js");
+var debug = require("./core/debug.js");
+var mawlabSock = require("./core/mawlabSock.js");
+var config = require('./db/config.js');
+var whois = require('./core/whois.js');
+
+
+// WebSockets
+mawlabSock.mawlab.get(); // iniciar
+
+whois.tracer();
+
+// Log
+//debug.log();
+
+var mona = "Manuel";
+// server
+console.log("Name Server: " + config.name);
+console.log("Version Server: " +config.version);
+console.log("Server Preparado");
