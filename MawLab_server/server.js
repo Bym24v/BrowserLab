f4db0cc769284db9e2d0eb1f9e7b69bdf2f05875
bym24v <manuel15986@hotmail.com>
2015-12-02 14:04:29 +0100

    TODO

--- MawLab_server/server.js
+++ MawLab_server/server.js
@@ -4,18 +4,26 @@
 var mawlabSock = require("./core/mawlabSock.js");
 var config = require('./db/config.js');
 var whois = require('./core/whois.js');
+var dnsResolver = require('./core/dnsResolver.js');
 var debug = require("./core/debug.js");
 
 
 // WebSockets
 mawlabSock.mawlab.get(); // iniciar
 
-whois.tracer();
+// Whois
+//whois.tracer();
+
+// dns Resolver
+//dnsResolver.get("google.com");
 
 // Log
-debug.log();
+//debug.log();
 
 // server
-console.log("Name Server: " + config.name);
-console.log("Version Server: " +config.version);
-console.log("Server Preparado");
+console.log("Server Preparado" +
+            "\n" + " " + "\n" +
+            "Server Name: " +
+            config.name + "\n" +
+            "Server Version: " + config.version);
+
